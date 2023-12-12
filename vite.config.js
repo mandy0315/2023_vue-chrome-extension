cb622817c481a629157021fe7a80d40e0ad39183
Mandy Tsai <mandy3152618@gmail.com>
2023-12-12 16:16:32 +0800

    build: 打包設定

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  
+  build: {
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, "popup.html"),
+      },
+      output: {
+        entryFileNames: `assets/js/[name].js`,
+        chunkFileNames: `assets/js/[name].js`,
+        assetFileNames: `[name].[ext]`,
+      }
+    }
+  }
 })
