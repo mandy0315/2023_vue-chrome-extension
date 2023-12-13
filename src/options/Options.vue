f1d5318bf3a9c28bb3d1d03a36d211004531cc9a
Mandy Tsai <mandy3152618@gmail.com>
2023-12-13 13:49:57 +0800

    build: 選項頁設定

---
+++ src/options/Options.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+
+<template>
+  <div class="text-xl text-red-500">Hello</div>
+</template>
--- vite.config.js
+++ vite.config.js
@@ -22,6 +22,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         popup: resolve(__dirname, "popup.html"),
+        options: resolve(__dirname, "options.html"),
         background: resolve(__dirname, 'src/background.js'),
       },
       output: {
