6f59e661b33646cba3f47c5b8bb4ef4c6c2fd132
Mandy Tsai <mandy3152618@gmail.com>
2023-12-13 16:40:58 +0800

    add: 改彈跳視窗

--- src/popup/popup.vue
+++ src/popup/popup.vue
@@ -1,6 +1,10 @@
 <script setup>
+const toOptions = () => {
+  chrome.runtime.openOptionsPage();
+};
 </script>
 
 <template>
   <div class="text-xl text-red-500">Hello</div>
+  <button @click="toOptions" class="border-2 rounded px-2">Go to options</button>
 </template>
