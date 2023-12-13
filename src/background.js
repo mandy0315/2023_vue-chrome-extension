9176b4cb644c2bb411b24ab802e7a59b2bd469ea
Mandy Tsai <mandy3152618@gmail.com>
2023-12-13 13:41:25 +0800

    add: getBadgeText 套件 icon 文字

--- src/background.js
+++ src/background.js
@@ -1,5 +1,17 @@
-// 初始狀態監聽
 chrome.runtime.onInstalled.addListener(() => {
   console.log('onInstalled...');
+  chrome.action.setBadgeText({
+    text: "OFF",
+  });
 });
 
+chrome.action.onClicked.addListener(async (tab) => {
+  const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+  console.log('prevState',prevState);
+  const nextState = prevState === 'ON' ? 'OFF' : 'ON'
+
+  await chrome.action.setBadgeText({
+    tabId: tab.id,
+    text: nextState,
+  });
+});
